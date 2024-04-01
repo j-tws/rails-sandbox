7660752fad334c78dfe6fe7476fb22a3c48791ab
Justin Tan <justintws10@gmail.com>
2024-04-01 16:46:58 +1100

    created todo list and item model

---
+++ db/migrate/20240401054441_add_list_to_item.rb
@@ -0,0 +1,5 @@
+class AddListToItem < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :items, :list, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_01_054441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "list_id", null: false
+    t.index ["list_id"], name: "index_items_on_list_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "items", "lists"
 end
