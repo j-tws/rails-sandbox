63d25df4b111954b02a3ffe491b36d7ea1bb1026
Justin Tan <justintws10@gmail.com>
2024-04-09 09:04:57 +1000

    added turbo frames & streams for frames, and tailwind

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,11 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
   def index
-    @lists = List.all
+    @lists = List.all.reverse
   end
 
   def show
-    @list = List.find(params[:id])
   end
 
   def new
@@ -15,19 +16,19 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
 
     if @list.save
-      redirect_to lists_path, notice: 'List successfully created.'
+      respond_to do |format|
+        format.html { redirect_to lists_path, notice: 'List successfully created.' }
+        format.turbo_stream
+      end
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @list = List.find(params[:id])
   end
 
   def update
-    @list = List.find(params[:id])
-
     if @list.update(list_params)
       redirect_to lists_path, notice: 'List successfully edited.'
     else
@@ -36,15 +37,21 @@ class ListsController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:id])
-
     @list.destroy
-    redirect_to lists_path
+
+    respond_to do |format|
+      format.html { redirect_to lists_path }
+      format.turbo_stream
+    end
   end
 
   private
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
